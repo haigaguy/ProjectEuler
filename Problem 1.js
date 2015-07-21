eb91d30f056c16fa417045458f444f275c6f34d0
Harshal Gupta <harshalgupta826@gmail.com>
2015-07-21 17:19:03 +0530

    Update Problem 1.js

--- Problem 1.js
+++ Problem 1.js
@@ -1,3 +1,6 @@
+//Problem 1 at Project Euler was to sum all the numbers divisible by 3 or 5 below 1000
+//Following is my code for the same
+
 var div3 = new Array();               //Array to store the numbers divisible by 3 or 5
 
 for( var x = 1; x < 1001; x++) {      // for loop to run the if statement below 1000 times
@@ -9,5 +12,5 @@ for( var x = 1; x < 1001; x++) {      // for loop to run the if statement below 
 
 }
 
-alert(eval(div3.join('+')));          // join function inside eval to dd all the numbers inside the array
+eval(div3.join('+')));          // join function inside eval to dd all the numbers inside the array
 console.log(eval);                    // displaying eval in a dialog box to the user
